2f7ac99f83a6cd43296605fba8678812c92c488d
Niall Carbery <niallcarbery0@gmail.com>
2023-08-07 21:23:17 +0100

    Nvim Personla set up Pyhton enabled

---
+++ README.md
@@ -0,0 +1,2 @@
+# NvimRc
+# NvimRc
---
+++ lua/niall/init.lua
@@ -0,0 +1,50 @@
+require("niall.set")
+require("niall.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('Niall', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = NiallGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf"
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --crlf",
+    ["*"] = "win32yank.exe -o --crlf"
+  },
+  cache_enable = 0,
+}
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = '/usr/bin/python'
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
